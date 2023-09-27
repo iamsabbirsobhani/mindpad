dd5ce2e0fd364d50376ac94674d23021f1d2603d
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-09-27 20:35:08 +0600

    finish homepage ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,46 @@ import { RegisterLink, LoginLink } from '@kinde-oss/kinde-auth-nextjs/server';
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>MindPad</h1>
-      <LoginLink>Sign in</LoginLink>
-      <RegisterLink>Sign up</RegisterLink>
+    <main className=" mt-[10vh]">
+      <div className="relative">
+        <div
+          className="mt-5 mb-2 m-auto "
+          style={{
+            backgroundImage:
+              'url(https://buxgpprwecixpghvntli.supabase.co/storage/v1/object/public/mindpad/assets/notebg.png)',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            height: '25vh',
+            width: '30vh',
+            opacity: '0.2',
+          }}
+        ></div>
+        <div className=" absolute left-0 right-0 top-0 bottom-0 w-[150px] m-auto flex justify-center items-center">
+          <Image
+            src="/images/logo.png"
+            alt="MindPad Logo"
+            width={150}
+            height={150}
+            className="object-contain "
+          />
+        </div>
+      </div>
+      <div className="text-center mt-5 mb-2">
+        <h1 className="font-light text-lg text-gray-500">
+          Your digital notepad for organizing your thoughts
+        </h1>
+      </div>
+      <div className="max-w-3xl  m-auto bg-no-repeat  flex flex-col justify-center items-center bg-opacity-20">
+        <div className=" flex flex-col ">
+          <button className=" bg-blue-500 font-bold text-gray-50 p-2 rounded-md w-48 self-center m-2 uppercase shadow-lg h-12">
+            <LoginLink>Sign in</LoginLink>
+          </button>
+          <button className=" backdrop-blur-md border p-2 rounded-md w-48 self-center m-2 uppercase shadow-lg font-bold h-12 text-gray-500">
+            <RegisterLink>Sign up</RegisterLink>
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className=" text-center mt-[10vh] mb-2">
+      <p className="font-light text-gray-500">&copy; 2023 - Sabbir Sobhani</p>
+    </div>
+  );
+}
