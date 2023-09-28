a5d10c89ceeb9f9353637e37b0c55912fd7714b7
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-09-28 10:44:18 +0600

    add search input to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,13 +9,44 @@ export default async function Note() {
   return (
     <>
       <Drawer />
-      <div className="ml-24">
-        <LogoutLink>
+      <div className="ml-24 mt-5">
+        {/* <LogoutLink>
           <button className="hover:bg-blue-600 transition-all duration-300 bg-blue-500 font-bold text-gray-50 p-2 rounded-md w-48 self-center m-2 uppercase shadow-lg h-10">
             Log out
           </button>
-        </LogoutLink>
-        <div></div>
+        </LogoutLink> */}
+        <div>
+          {/* note search */}
+          <div className=" relative">
+            <input
+              type="search"
+              className="focus:outline-none ml-7"
+              placeholder="Search"
+            />
+            <div className="absolute flex justify-center items-center top-0  bottom-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5 text-gray-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </div>
+          </div>
+          {/* profile */}
+          <div>
+            <div className=" relative">
+              <button className=""></button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
