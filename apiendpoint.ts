088ae0784a70de5cb0bf0a46de7e99357024b87b
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-09-30 12:33:38 +0600

    change apiendpoint to prod

--- apiendpoint.ts
+++ apiendpoint.ts
@@ -1,2 +1,2 @@
-// export const API = 'https://mindpad.vercel.app';
-export const API = 'http://localhost:3000s';
+export const API = 'https://mindpad.vercel.app';
+// export const API = 'http://localhost:3000';
